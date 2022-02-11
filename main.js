998870bcd1866949931a6646bec84db2fc38ba0a
AdiNayak <70101982+Adisha-Nayak@users.noreply.github.com>
2022-02-11 09:05:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var firstname="Adisha";
+var lastname="Nayak";
+var fullname= firstname.concat(lastname);
+console.log(fullname);
+var sqroot= Math.sqrt(81);
+console.log(sqroot);
+var maximum_number = Math.max(45,4);
+console.log(maximum_number);
+var minimum_number = Math.min(67,98);
+console.log(minimum_number);
+var powerM = Math.pow(9,9);
+console.log(powerM);
